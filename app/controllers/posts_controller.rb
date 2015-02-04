36d20fd19565c900dfd7949460daa33e84305f52
Mansurbek T <mansur.telemuratov@gmail.com>
2015-02-04 19:27:58 +0300

    today

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+	def index
+		@content_first = 'This is a sample blog web page designed by mansur'
+		@content_second = 'This is a sample blog wesdfb page designed by mansur'
+	end
+	
+	def new
+	
+	end
+	
+	def create
+	
+	end
+	
+	def edit
+	
+	end
+	
+	def update
+	
+	end
+	
+	def show
+	
+	end
+	
+	def destroy
+	
+	end
+end
